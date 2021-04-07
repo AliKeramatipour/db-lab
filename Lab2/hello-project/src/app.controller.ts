f27531299ce5a62866c6851a19555562776967cc
alikeramatipour <alikeramatipour@yahoo.com>
2021-04-07 17:23:34 +0430

    completed step 1

--- Lab2/hello-project/src/app.controller.ts
+++ Lab2/hello-project/src/app.controller.ts
@@ -4,20 +4,4 @@ import { AppService } from './app.service';
 @Controller('/hello')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Get("/bye")
-  getBye(): string {
-    return this.appService.getHello();
-  }
-
-  @Get("/ali")
-  getAli(): string {
-    return this.appService.getHello();
-  }
-
 }

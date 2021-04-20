a180d7f48ca9aa26fe53018fa34001ab50969754
alikeramatipour <alikeramatipour@yahoo.com>
2021-04-20 18:23:32 +0430

    added and debugged codes from codersera webpage

---
+++ Lab2/hello-project/src/user/user.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Header, Get, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { UserService } from './user.service';
+import CreateUserDto from './dto/create-user.dto';
+import { ApiResponse, ApiBody } from '@nestjs/swagger';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly usersService: UserService) {}
+
+//'postUser()' will handle the creating of new User
+  @Post('post')
+  postUser( @Body() user: CreateUserDto) {
+    return this.usersService.insert(user);
+  }
+
+// 'getAll()' returns the list of all the existing users in the database
+  @Get()
+  getAll() {
+    return this.usersService.getAllUsers();
+  }
+
+//'getBooks()' return all the books which are associated with the user 
+// provided through 'userID' by the request  
+  @Get('books')
+  getBooks( @Body('userID', ParseIntPipe) userID: number ) {
+    return this.usersService.getBooksOfUser(userID);
+  }
+}
